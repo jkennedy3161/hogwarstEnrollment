9f9074080363528f75fc5bc17bbdb2768ce7f8d5
jkennedy3161 <kennedyjt88@gmail.com>
2017-08-04 14:51:37 -0700

    add route endpoint for students

--- server/api/api.js
+++ server/api/api.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const houseRouter = require('./routes/houseRoutes');
+const studentRouter = require('./routes/studentRoutes');
 // hook different model routes here
 router.use('/houses', houseRouter);
+router.use('/students', studentRouter);
 
 module.exports = router;
