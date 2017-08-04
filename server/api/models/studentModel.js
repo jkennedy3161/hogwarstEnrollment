15e6d722cc16427d720cf94c886075b38341afb0
jkennedy3161 <kennedyjt88@gmail.com>
2017-08-04 14:52:20 -0700

    add model schema for students

---
+++ server/api/models/studentModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const StudentSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  house: {
+    type:Schema.Types.ObjectId,
+    ref: 'house'
+  }
+});
+
+module.exports = mongoose.model('student', StudentSchema);
